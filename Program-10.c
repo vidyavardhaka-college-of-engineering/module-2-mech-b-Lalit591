e42becce025d3d19ccafdc2cb0e7fa69c2f980d6
Lalit591 <20me0005@vvce.ac.in>
2021-03-07 15:58:32 +0000

    program is done

--- Program-10.c
+++ Program-10.c
@@ -26,14 +26,14 @@ int main()
             small = arr[i];//missing code
         }
         
-        if (arr[i] >large  )//missing code
+        if (arr[i] >large)//missing code
         {
-            large= arr[];//missing code
+            large= arr[i];//missing code
         }
     }
 
     printf("The largest element of the array = %d", large);
-    printf("The smallest element of the array = %d",);
+    printf("The smallest element of the array = %d",small);
 
     return 0;
 }
