d650607a3e0982adafe96c4de98ec31e1e50896b
Lalit591 <20me0005@vvce.ac.in>
2021-03-07 15:16:53 +0000

    program is done

--- Program-3.c
+++ Program-3.c
@@ -5,13 +5,14 @@
 #include <stdio.h>
 int main() {
 	int i = 1, n;
-	printf("Enter an integer");
+	printf("Enter an integer",n);
 	scanf("%d",&n);
  // Fill the code in scanf()
 	do {
 		printf("I Love VVCE ");
 		i++;
 		// complete the statement
-	} while(i<=);
-	return(0);// complete the condition
+	} while(i<=n);
+	return(0);
+  // complete the condition
 }
